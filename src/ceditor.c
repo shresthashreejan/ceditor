e78af237e1c0b87d19acd198c5b32cf5ab4d9662
shreejan shrestha <shr.shreejan@gmail.com>
2025-01-18 21:23:19 +0545

    Added changes for clearing screen and repositioning cursor

--- src/ceditor.c
+++ src/ceditor.c
@@ -1,3 +1,4 @@
+// Includes
 #include <ctype.h>
 #include <errno.h>
 #include <stdio.h>
@@ -5,9 +6,17 @@
 #include <termios.h>
 #include <unistd.h>
 
+// Defines
+#define CTRL_KEY(k) ((k) & 0x1f)
+
+// Data
 struct termios original_termios;
 
+// Terminal
 void die(const char *s) {
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     perror(s);
     exit(1);
 }
@@ -45,17 +54,56 @@ void enableRawMode() {
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) == -1) die("tcsetattr");
 }
 
+char editorReadKey() {
+    int nread;
+    char c;
+    while ((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if (nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+// Output
+
+void editorDrawRows() {
+    int y;
+    for (y=0;y<24;y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+void editorRefreshScreen() {
+    /*
+        - Escape sequences always start with an escape character (27 or \x1b) followed by a [ character
+    */
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    /*
+        - Escape sequence to reposition cursor at the top left corner
+    */
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+// Input
+void editorProcessKeypress() {
+    char c = editorReadKey();
+    switch(c) {
+        case CTRL_KEY('q'):
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
+    }
+}
+
+// Init
 int main() {
     enableRawMode();
     while(1) {
-        char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if(iscntrl(c)) {
-            printf("%d\r\n", c);
-        } else {
-            printf("%d (%c)\r\n", c, c);
-        }
-        if(c == 'q') break;
+        editorRefreshScreen();
+        editorProcessKeypress();
     }
     return 0;
 }
